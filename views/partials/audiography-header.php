24f2995892fbad681058e49ec92317bae4f454a7
Jeff Everhart <Jeffeverhart383@gmail.com>
2017-09-11 12:54:42 -0400

    revert

--- views/partials/audiography-header.php
+++ views/partials/audiography-header.php
@@ -3,7 +3,7 @@
 	<div class="container-fluid">
 		<div class="row">
 		<div class="col-lg-12">
-			<h1>Audiography Plugin | GitHub Test 2</h1>
+			<h1>Audiography Plugin</h1>
 
 	<ul class="nav nav-pills">
 		<li role="presentation">
